4358d97cd3cd46a2b2ea303ad0476d78285ba469
JamesThePaladin <Jamesthepaladin@gmail.com>
2020-07-03 12:45:29 -0700

    Sort of added  controller
    Began the controller script, ending it here to do a seperate tutorial

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health;
+
+    public void TakeDamage(float amount) 
+    {
+        health -= amount;
+        if (health <= 0) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
